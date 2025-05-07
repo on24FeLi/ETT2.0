f0e65bc1b5f55b62ab4c31d7c1de1fa6ec893e98
stuetzleanika <anika.stuetzle@icloud.com>
2025-05-07 09:37:45 +0200

    Added Navigation  to Urlaub

--- src/views/Urlaub.vue
+++ src/views/Urlaub.vue
@@ -1,9 +1,37 @@
 <script setup>
-
+import { ref, computed } from "vue";
+import navigation from '@/components/navigation.vue';
 </script>
 <template>
+     <div>
+      <header>
+      <h1>Urlaub</h1>
+      <navigation/>
+      </header>
+     </div>
 <p>Urlaub  // Hier kommt der HTML Part rein</p>
 </template>
 <style scoped>
+    header {
+  background-color: #f1ecdb;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  border-bottom: 1px solid #ccc;
+}
 
+header h1 {
+  font-size: 2rem;
+  padding: 0.2rem 1rem;
+  display: inline-block;
+}
+
+.logo {
+  font-family: monospace;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
 </style>
